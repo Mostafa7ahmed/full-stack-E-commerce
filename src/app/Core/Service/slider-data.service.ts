187ba2e6a55e22b1603ae01ec58d644ce36d8c46
mostafa hamed <Mostafa7ahmed>
2024-09-21 05:16:29 +0300

    Ts Slider

--- src/app/Core/Service/slider-data.service.ts
+++ src/app/Core/Service/slider-data.service.ts
@@ -14,9 +14,14 @@ export class SliderDataService {
       buttonText: 'اطلب الأن',
       isActive: true 
     },
-
-
-
+    {
+      background: 'assets/Image/Homebanner.png',
+      title: 'اكتشف مجموعتنا الجديدة',
+      subtitle: 'جديد',
+      description: 'أكتشف افضل منتجاتنا الجديدة والراقية التي تناسب ذوقك.',
+      buttonText: 'اطلب الأن',
+      isActive: false 
+    },
     {
       background: 'assets/Image/Homebanner.png',
       title: 'اكتشف مجموعتنا الجديدة',
