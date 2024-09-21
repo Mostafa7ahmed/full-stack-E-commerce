68c4772d2ef636ad73dedaf7f68a2e340e3e9ef9
mostafa hamed <Mostafa7ahmed>
2024-09-21 04:57:48 +0300

    Data Slider

--- src/app/Core/Service/slider-data.service.ts
+++ src/app/Core/Service/slider-data.service.ts
@@ -15,43 +15,7 @@ export class SliderDataService {
       isActive: true 
     },
 
-    {
-      background: 'assets/Image/Homebanner.png',
-      title: 'اكتشف مجموعتنا الجديدة',
-      subtitle: 'جديد',
-      description: 'أكتشف افضل منتجاتنا الجديدة والراقية التي تناسب ذوقك.',
-      buttonText: 'اطلب الأن',
-      isActive: false 
-    },
 
-    {
-      background: 'assets/Image/Homebanner.png',
-      title: 'اكتشف مجموعتنا الجديدة',
-      subtitle: 'جديد',
-      description: 'أكتشف افضل منتجاتنا الجديدة والراقية التي تناسب ذوقك.',
-      buttonText: 'اطلب الأن',
-      isActive: false 
-    },
-
-
-    {
-      background: 'assets/Image/Homebanner.png',
-      title: 'اكتشف مجموعتنا الجديدة',
-      subtitle: 'جديد',
-      description: 'أكتشف افضل منتجاتنا الجديدة والراقية التي تناسب ذوقك.',
-      buttonText: 'اطلب الأن',
-      isActive: false 
-    },
-
-
-    {
-      background: 'assets/Image/Homebanner.png',
-      title: 'اكتشف مجموعتنا الجديدة',
-      subtitle: 'جديد',
-      description: 'أكتشف افضل منتجاتنا الجديدة والراقية التي تناسب ذوقك.',
-      buttonText: 'اطلب الأن',
-      isActive: false 
-    },
 
     {
       background: 'assets/Image/Homebanner.png',
