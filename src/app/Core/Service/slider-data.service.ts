4de8efc523303b47ef805e9fabd15b4e53e1b8b5
mostafa hamed <Mostafa7ahmed>
2024-09-23 17:39:47 +0300

    use service

--- src/app/Core/Service/slider-data.service.ts
+++ src/app/Core/Service/slider-data.service.ts
@@ -12,35 +12,32 @@ export class SliderDataService {
       subtitle: 'جديد',
       description: 'أكتشف افضل منتجاتنا الجديدة والراقية التي تناسب ذوقك.',
       buttonText: 'اطلب الأن',
-      isActive: true 
+      isActive: true  // First slide active by default
     },
     {
-      background: 'assets/Image/Homebanner.png',
-      title: 'اكتشف مجموعتنا الجديدة',
-      subtitle: 'جديد',
-      description: 'أكتشف افضل منتجاتنا الجديدة والراقية التي تناسب ذوقك.',
-      buttonText: 'اطلب الأن',
-      isActive: false 
+      background: 'assets/Image/Homebanner2.png',
+      title: 'استمتع بأحدث التقنيات',
+      subtitle: 'تكنولوجيا',
+      description: 'استمتع بمنتجاتنا التقنية المصممة لتلبية احتياجاتك اليومية.',
+      buttonText: 'تسوق الآن',
+      isActive: false
     },
     {
-      background: 'assets/Image/Homebanner.png',
-      title: 'اكتشف مجموعتنا الجديدة',
-      subtitle: 'جديد',
-      description: 'أكتشف افضل منتجاتنا الجديدة والراقية التي تناسب ذوقك.',
-      buttonText: 'اطلب الأن',
-      isActive: false 
+      background: 'assets/Image/Homebanner3.png',
+      title: 'خصومات هائلة جدا',
+      subtitle: 'خصومات',
+      description: 'اغتنم الفرصة واحصل على خصومات تصل إلى 50٪ على منتجات مختارة.',
+      buttonText: 'اكتشف المزيد',
+      isActive: false
     },
-
-
     {
-      background: 'assets/Image/Homebanner.png',
-      title: 'اكتشف مجموعتنا الجديدة',
-      subtitle: 'جديد',
-      description: 'أكتشف افضل منتجاتنا الجديدة والراقية التي تناسب ذوقك.',
-      buttonText: 'اطلب الأن',
-      isActive: false 
-    },
-
+      background: 'assets/Image/Homebanner3.png',
+      title: ' المنتجات بجودة عالية',
+      subtitle: 'مميز',
+      description: 'احصل على منتجات مميزة بجودة عالية وبأسعار تنافسية.',
+      buttonText: 'تسوق الآن',
+      isActive: false
+    }
   ];
 
   constructor() { }
